07d08ab491adeb746666f9df12c68e7be5f5dc26
Uqib3 <yyagiz828@gmail.com>
2026-08-29 18:12:38 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'study-flow-ipad-v1';
+const CACHE = 'study-flow-ipad-v2';
 const CORE = ['./index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
@@ -6,7 +6,11 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
